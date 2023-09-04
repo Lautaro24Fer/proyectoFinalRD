ef09ae3b8f2dd30f8292d7bc5b975ac323e200db
Lautaro <LautaroFer456@gmail.com>
2023-09-04 16:46:50 -0300

    Asides hechos - faltan otros index

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const ancho = window.innerWidth;
+const body = document.body;
+
+//FUNCIONALIDAD DE LAS CARD
 const botonesCambiarOpacidad = document.querySelectorAll(".card-btn");
       botonesCambiarOpacidad.forEach(function (boton) {
       boton.addEventListener("click", function () {
@@ -16,3 +20,26 @@ const botonesCambiarOpacidad = document.querySelectorAll(".card-btn");
         });
       });
     });
+
+//TRANSICION DEL SEARCHBAR
+const searchBtn = document.getElementById("navbar-btnSearch");
+const closeSearchBtn = document.getElementById("closeAsideBtn");
+const bandejaSearch = document.getElementsByClassName("lf__section-asides-search")[0];
+searchBtn.addEventListener("click", function(){
+  bandejaSearch.classList.add("asides-show");
+});
+closeSearchBtn.addEventListener("click", function(){
+  bandejaSearch.classList.remove("asides-show");
+});
+
+//TRANSICION DEL MENU
+const menuBtn = document.getElementById("navbar-btnMenu");
+const closeMenuBtn = document.getElementById("closeMenuBtn");
+const bandejaMenu = document.getElementsByClassName("lf__section-asides-menu")[0];
+
+menuBtn.addEventListener("click", function(){
+  bandejaMenu.classList.add("asides-show-menu");
+});
+closeMenuBtn.addEventListener("click", function(){
+  bandejaMenu.classList.remove("asides-show-menu");
+});
